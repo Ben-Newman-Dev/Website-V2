3f464f7190dbcccd4b1e3e10348b602ed87f053b
jam1nb3n <benewman2002@gmail.com>
2020-11-20 10:07:39 +0000

    header dynamic styling

--- react/bnewmandotdev/src/components/Global/Navigation.jsx
+++ react/bnewmandotdev/src/components/Global/Navigation.jsx
@@ -1,25 +1,39 @@
 const Navigation = () => {
+	let url = window.location.pathname;
+	let a = {};
+	let b = {};
+	let c = {};
+	let d = {};
+	let e = {};
+	let f = {};
+	switch (url) {
+		case "/about":
+			b = { backgroundColor: "#ef7c1a" };
+			break;
+		default:
+			a = { backgroundColor: "#ef7c1a" };
+	}
 	return (
 		<div className="navigation-wrapper">
 			<img src="https://via.placeholder.com/121x121.png" alt="Logo" />
 			<h1>BNewman.dev</h1>
 			<div className="link-wrapper">
-				<a className="home" href="/">
+				<a style={a} className="home" href="/">
 					Home
 				</a>
-				<a className="about" href="/about">
+				<a style={b} className="about" href="/about">
 					About Me
 				</a>
-				<a className="projects" href="/">
+				<a style={c} className="projects" href="/">
 					My Projects
 				</a>
-				<a className="devdiary" href="/">
+				<a style={d} className="devdiary" href="/">
 					DevDiary
 				</a>
-				<a className="links" href="/">
+				<a style={e} className="links" href="/">
 					Links
 				</a>
-				<a className="contact" href="/">
+				<a style={f} className="contact" href="/">
 					Contact Me
 				</a>
 			</div>
--- react/bnewmandotdev/src/pages/AboutMe.jsx
+++ react/bnewmandotdev/src/pages/AboutMe.jsx
@@ -1,6 +1,7 @@
 import Main from "../components/AboutMe/Main";
 import Footer from "../components/Global/Footer";
 import Navigation from "../components/Global/Navigation";
+import "../Styles/aboutme/styles.scss";
 
 const AboutMe = () => {
 	return (
